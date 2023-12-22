5a733a6dbe5159e98365303e08c21bc15c1bc94b
causerie <vanessaca@web.de>
2023-12-22 21:01:47 +0100

    tests mouse behavior triggering event AttributeErrors

--- kivy_garden/matplotlib/tests/test_events.py
+++ kivy_garden/matplotlib/tests/test_events.py
@@ -13,6 +13,49 @@ class TestEvents(GraphicUnitTest):
         ax.plot([1, 2, 3, 4])
         cls.figure_canvas = FigureCanvasKivyAgg(figure=fig)
 
-    def test_screen(self):
+    def test_resize_screen(self):
         self.render(self.figure_canvas)
 
+    def test_touch_down(self):
+        self.render(self.figure_canvas)
+        UnitTestTouch(0.5, 0.5).touch_down()
+
+    def test_touch_down_scrolldown(self):
+        self.render(self.figure_canvas)
+        touch = UnitTestTouch(0, 0)
+        touch.profile = 'button'
+        touch.button = 'scrolldown'
+        touch.touch_down()
+
+    def test_touch_down_scrolluo(self):
+        self.render(self.figure_canvas)
+        touch = UnitTestTouch(0, 0)
+        touch.profile = 'button'
+        touch.button = 'scrollup'
+        touch.touch_down()
+
+    def test_touch_up(self):
+        self.render(self.figure_canvas)
+        touch = UnitTestTouch(0.5, 0.5)
+        touch.touch_down()
+        touch.touch_up()
+
+    def test_touch_up_scrolldown(self):
+        self.render(self.figure_canvas)
+        touch = UnitTestTouch(0, 0)
+        touch.profile = 'button'
+        touch.button = 'scrolldown'
+        touch.touch_down()
+        touch.touch_up()
+
+    def test_touch_up_scrolluo(self):
+        self.render(self.figure_canvas)
+        touch = UnitTestTouch(0, 0)
+        touch.profile = 'button'
+        touch.button = 'scrollup'
+        touch.touch_down()
+        touch.touch_up()
+
+    def test_touch_move(self):
+        self.render(self.figure_canvas)
+        UnitTestTouch(0.5, 0.5).touch_move(0.5, 0.5)

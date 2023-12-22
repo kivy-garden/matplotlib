47d4c90aad42bd1fa6f2faf274ec32e0bcf1242e
causerie <vanessaca@web.de>
2023-12-22 19:35:26 +0100

    deprecation warning for matplotlib 3.8

--- kivy_garden/matplotlib/tests/test_backend_kivyagg.py
+++ kivy_garden/matplotlib/tests/test_backend_kivyagg.py
@@ -2,6 +2,7 @@ def test_matplotlib_use_backend():
     import matplotlib
     import matplotlib.pyplot as plt
 
+    plt.close('all')
     matplotlib.use("module://kivy_garden.matplotlib.backend_kivyagg")
 
     plt.plot([1, 2, 3, 4])
